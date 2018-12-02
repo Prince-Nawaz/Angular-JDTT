2dca3c4aa2ab4a3fbfcb4353802e43e32519a052
Prince Nawaz <princenawaz007@gmail.com>
2018-12-02 21:37:32 +0530

    Added firebase for production mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAjDLQxyZFdhg_Lcv6rr0ako6DFrcITDX4',
+    authDomain: 'jdtt-tpt.firebaseapp.com',
+    databaseURL: 'https://jdtt-tpt.firebaseio.com',
+    projectId: 'jdtt-tpt',
+    storageBucket: 'jdtt-tpt.appspot.com',
+    messagingSenderId: '823543039103'
+  }
 };
